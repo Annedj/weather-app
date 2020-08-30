c6b2d4a67fd39a176a5f9a17e6c8f54cf830e4b8
Anne de Joly <anne.dejoly@edhec.com>
2020-08-30 18:26:22 +0100

    Styled and made responsive the input buttons

--- app/javascript/components/getLocationWeather.js
+++ app/javascript/components/getLocationWeather.js
@@ -1,28 +1,29 @@
 import { updateWeather, updateCity } from "./updatePage";
 
 export default () => {
-  const button = document.getElementById('get-location');
-  const inputs = document.querySelector('.inputs');
+  const button = document.getElementById("get-location");
+  const inputs = document.querySelector(".inputs");
 
   if (button) {
-    button.addEventListener("click", (event) => {
+    button.addEventListener("click", event => {
       event.preventDefault();
-      event.target.outerHTML = `<button class="btn btn-primary round mr-2" type="button" disabled>
+      event.target.outerHTML = `<button class="btn btn-primary round" type="button" disabled>
         <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Loading...</button>`;
 
       navigator.geolocation.getCurrentPosition(
         geoLocationSuccess,
         geoLocationError
       );
+      console.log(inputs);
       setTimeout(() => {
-        inputs.children[0].style.display = 'none';
-        inputs.children[1].style.display = 'none';
+        inputs.children[0].style.display = "none";
+        // inputs.children[1].style.display = "none";
       }, 2000);
     });
-  };
+  }
 };
 
-const cityName = async (coordinates) => {
+const cityName = async coordinates => {
   const response = await fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&localityLanguage=en`
   );
@@ -36,8 +37,8 @@ const locationWeather = async (latitude, longitude) => {
   return await response.json();
 };
 
-export const geoLocationSuccess = async (position) => {
-  console.log('Position: ', position);
+export const geoLocationSuccess = async position => {
+  console.log("Position: ", position);
   updateCity(await cityName(position.coords));
   updateWeather(
     await locationWeather(position.coords.latitude, position.coords.longitude)
@@ -46,4 +47,4 @@ export const geoLocationSuccess = async (position) => {
 
 export const geoLocationError = () =>
   // button.insertAdjacentText("beforebegin", "Not available");
-  window.alert('Location not found');
+  window.alert("Location not found");
